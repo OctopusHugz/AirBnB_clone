a008477e57111d28c1beda18c1a15e90090ee06f
GunterPearson <1715@holbertonschool.com>
2020-06-30 20:30:44 +0000

    added unfinished test_user file

---
+++ tests/test_models/test_user.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+""" Test State Module """
+import unittest
+from models.user import User
+import datetime
+
+
+class TestUser(unittest.TestCase):
+    """ Unit Tests for User Class """
+    def setUp(self):
+        """setup for test of user"""
+        pass
+
+    def test_setup(self):
+        """test setup of User"""
+        pass
+
+    def test_types(self):
+        """test Types"""
+        pass
+
+    def test_save(self):
+        """test save """
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
