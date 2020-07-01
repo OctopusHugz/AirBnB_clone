fb49bc461fe5404ef18a5009e32f649fa495e06d
GunterPearson <1715@holbertonschool.com>
2020-07-01 19:24:14 +0000

    pushing new tests for console

--- tests/test_console.py
+++ tests/test_console.py
@@ -9,16 +9,64 @@ from io import StringIO
 from unittest.mock import patch
 
 
-class TestHBNBCommand_empty_prompt(unittest.TestCase):
+class TestConsole(unittest.TestCase):
     """test empty line from console"""
 
     def test_prompt(self):
+        """ test prompt"""
         self.assertEqual("(hbnb) ", HBNBCommand.prompt)
 
     def test_empty(self):
+        """ test empty line"""
         with patch("sys.stdout", new=StringIO()) as out:
             self.assertEqual("", out.getvalue().strip())
 
+    def test_quit(self):
+        """ test quit function"""
+        with patch("sys.stdout", new=StringIO) as out:
+            self.assertTrue(HBNBCommand().onecmd("quit"))
+
+    def test_all(self):
+        """ test all function"""
+        string = "** class doesn't exist **"
+        with patch("sys.stdout", new=StringIO()) as out:
+            self.assertFalse(HBNBCommand().onecmd("all Model"))
+            self.assertEqual(string, out.getvalue().strip())
+
+    def test_help_quit(self):
+        """ test quit from help function"""
+        string = "Quit command to exit the console"
+        with patch("sys.stdout", new=StringIO()) as out:
+            self.assertFalse(HBNBCommand().onecmd("help quit"))
+            self.assertEqual(string, out.getvalue().strip())
+
+    def test_help_EOF(self):
+        """ test EOF from help"""
+        string = "EOF command to exit the console"
+        with patch("sys.stdout", new=StringIO()) as out:
+            self.assertFalse(HBNBCommand().onecmd("help EOF"))
+            self.assertEqual(string, out.getvalue().strip())
+
+    def test_show(self):
+        """ test show from console"""
+        string = "** class name missing **"
+        with patch("sys.stdout", new=StringIO()) as out:
+            self.assertFalse(HBNBCommand().onecmd("show"))
+            self.assertEqual(string, out.getvalue().strip())
+
+    def test_update(self):
+        """ test update from console"""
+        string = "** class name missing **"
+        with patch("sys.stdout", new=StringIO()) as out:
+            self.assertFalse(HBNBCommand().onecmd("update"))
+            self.assertEqual(string, out.getvalue().strip())
+
+    def test_destroy(self):
+        """ test destroy from console"""
+        string = "** class name missing **"
+        with patch("sys.stdout", new=StringIO()) as out:
+            self.assertFalse(HBNBCommand().onecmd("destroy"))
+            self.assertEqual(string, out.getvalue().strip())
 
 if __name__ == "__main__":
     unittest.main()
