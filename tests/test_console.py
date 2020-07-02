19f03b1604c19a781f1d150f2851a4095c212dc2
GunterPearson <1715@holbertonschool.com>
2020-07-02 15:00:15 +0000

    pushing new test to test console

--- tests/test_console.py
+++ tests/test_console.py
@@ -75,47 +75,5 @@ class TestConsole(unittest.TestCase):
             self.assertFalse(HBNBCommand().onecmd("create"))
             self.assertEqual(string, out.getvalue().strip())
 
-    def test_BaseModel_all(self):
-        """test BaseModel all"""
-        with patch("sys.stdout", new=StringIO()) as out:
-            self.assertFalse(HBNBCommand().onecmd("BaseModel.all()"))
-            self.assertEqual(str, type(out.getvalue()))
-
-    def test_Review_all(self):
-        """test BaseModel all"""
-        with patch("sys.stdout", new=StringIO()) as out:
-            self.assertFalse(HBNBCommand().onecmd("Review.all()"))
-            self.assertEqual(str, type(out.getvalue()))
-
-    def test_User_all(self):
-        """test BaseModel all"""
-        with patch("sys.stdout", new=StringIO()) as out:
-            self.assertFalse(HBNBCommand().onecmd("User.all()"))
-            self.assertEqual(str, type(out.getvalue()))
-
-    def test_State_all(self):
-        """test BaseModel all"""
-        with patch("sys.stdout", new=StringIO()) as out:
-            self.assertFalse(HBNBCommand().onecmd("State.all()"))
-            self.assertEqual(str, type(out.getvalue()))
-
-    def test_City_all(self):
-        """test BaseModel all"""
-        with patch("sys.stdout", new=StringIO()) as out:
-            self.assertFalse(HBNBCommand().onecmd("City.all()"))
-            self.assertEqual(str, type(out.getvalue()))
-
-    def test_Amenity_all(self):
-        """test BaseModel all"""
-        with patch("sys.stdout", new=StringIO()) as out:
-            self.assertFalse(HBNBCommand().onecmd("Amenity.all()"))
-            self.assertEqual(str, type(out.getvalue()))
-
-    def test_Place_all(self):
-        """test BaseModel all"""
-        with patch("sys.stdout", new=StringIO()) as out:
-            self.assertFalse(HBNBCommand().onecmd("Place.all()"))
-            self.assertEqual(str, type(out.getvalue()))
-
 if __name__ == "__main__":
     unittest.main()
