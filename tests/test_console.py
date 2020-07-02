d5f7e60c53830d24f52f64497c1bafb28dcec71c
GunterPearson <1715@holbertonschool.com>
2020-07-02 14:48:55 +0000

    pushing new test to test console

--- tests/test_console.py
+++ tests/test_console.py
@@ -16,7 +16,7 @@ class TestConsole(unittest.TestCase):
         """ test prompt"""
         self.assertEqual("(hbnb) ", HBNBCommand.prompt)
 
-    def test_empty(self):
+    def test_empty_line(self):
         """ test empty line"""
         with patch("sys.stdout", new=StringIO()) as out:
             self.assertEqual("", out.getvalue().strip())
@@ -67,6 +67,55 @@ class TestConsole(unittest.TestCase):
         with patch("sys.stdout", new=StringIO()) as out:
             self.assertFalse(HBNBCommand().onecmd("destroy"))
             self.assertEqual(string, out.getvalue().strip())
+
+    def test_create(self):
+        """test create from console"""
+        string = "** class name missing **"
+        with patch("sys.stdout", new=StringIO()) as out:
+            self.assertFalse(HBNBCommand().onecmd("create"))
+            self.assertEqual(string, out.getvalue().strip())
+
+    def test_BaseModel_all(self):
+        """test BaseModel all"""
+        with patch("sys.stdout", new=StringIO()) as out:
+            self.assertFalse(HBNBCommand().onecmd("BaseModel.all()"))
+            self.assertEqual(str, type(out.getvalue()))
+
+    def test_Review_all(self):
+        """test BaseModel all"""
+        with patch("sys.stdout", new=StringIO()) as out:
+            self.assertFalse(HBNBCommand().onecmd("Review.all()"))
+            self.assertEqual(str, type(out.getvalue()))
+
+    def test_User_all(self):
+        """test BaseModel all"""
+        with patch("sys.stdout", new=StringIO()) as out:
+            self.assertFalse(HBNBCommand().onecmd("User.all()"))
+            self.assertEqual(str, type(out.getvalue()))
+
+    def test_State_all(self):
+        """test BaseModel all"""
+        with patch("sys.stdout", new=StringIO()) as out:
+            self.assertFalse(HBNBCommand().onecmd("State.all()"))
+            self.assertEqual(str, type(out.getvalue()))
+
+    def test_City_all(self):
+        """test BaseModel all"""
+        with patch("sys.stdout", new=StringIO()) as out:
+            self.assertFalse(HBNBCommand().onecmd("City.all()"))
+            self.assertEqual(str, type(out.getvalue()))
+
+    def test_Amenity_all(self):
+        """test BaseModel all"""
+        with patch("sys.stdout", new=StringIO()) as out:
+            self.assertFalse(HBNBCommand().onecmd("Amenity.all()"))
+            self.assertEqual(str, type(out.getvalue()))
+
+    def test_Place_all(self):
+        """test BaseModel all"""
+        with patch("sys.stdout", new=StringIO()) as out:
+            self.assertFalse(HBNBCommand().onecmd("Place.all()"))
+            self.assertEqual(str, type(out.getvalue()))
 
 if __name__ == "__main__":
     unittest.main()
